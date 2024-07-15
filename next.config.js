5c74ed9422d229341cb1ad238aec5b65e480a3b0
Carlos <carlosd.dev@gmail.com>
2024-07-15 16:41:50 -0400

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const path = require('path');
+const withTM = require('next-transpile-modules')([]);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack(config) {
+		config.resolve.alias['@'] = path.join(__dirname, 'src');
+		return config;
+	}
+};
 
 module.exports = nextConfig;
